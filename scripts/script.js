b15ba52fe35632b0c544c9bfcf29e795b728dbb3
Foraminiferatrv <ione.trv@gmail.com>
2019-04-09 01:55:31 +0300

    game's links

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,31 @@
-$(document).ready(function () {
-  var slides = document.querySelectorAll('.slide');
-  console.log({
+$( document ).ready( function () {
+  var slides = document.querySelectorAll( '.slide' );
+  console.log( {
     slides: slides
-  });
-  $('.slider').slick({
+  } );
+  $( '.slider' ).slick( {
     arrows: true,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 8000,
     fade: true,
-    pauseOnHover: true
-  });
-  var div = document.querySelector('.news');
-  var news = content[0];
-  var pFirst = document.createElement('p');
+    pauseOnHover: true,
+  } );
+  var div = document.querySelector( '.news' );
+  var news = content[ 0 ];
+  var pFirst = document.createElement( 'p' );
   pFirst.innerText = news;
-  div.append(pFirst);
+  div.append( pFirst );
 
-  
-}
-);
 
-$.getJSON('../assets/data/card-data.json', (userData)=>{
-  userData.forEach(user=>{
-    addCard(user);
-  })
-  console.log(userData);
- 
-});
+} );
+
+$.getJSON( '../assets/data/card-data.json', ( userData ) => {
+  userData.forEach( user => {
+    addCard( user );
+  } )
+  console.log( userData );
+
+} );
 
 var content = [
   'Ahoy! We have come into the Hearthstone!',
@@ -36,33 +35,33 @@ var content = [
   'Some of us are playing even in PUBG... You may join!'
 ];
 
-$('.slider').on('afterChange', function (event) {
-  var slides = document.querySelectorAll('.slide');
-  slides = Array.from(slides).filter(function (slide) {
-    return !slide.classList.contains("slick-cloned");
-  });
+$( '.slider' ).on( 'afterChange', function ( event ) {
+  var slides = document.querySelectorAll( '.slide' );
+  slides = Array.from( slides ).filter( function ( slide ) {
+    return !slide.classList.contains( "slick-cloned" );
+  } );
 
-  var current = Array.from(slides).find(slide => slide.classList.contains('slick-current'));
-  const index = Array.from(slides).indexOf(current);
-  const newsContent = content[index];
-  var pNews = document.createElement('p');
+  var current = Array.from( slides ).find( slide => slide.classList.contains( 'slick-current' ) );
+  const index = Array.from( slides ).indexOf( current );
+  const newsContent = content[ index ];
+  var pNews = document.createElement( 'p' );
   pNews.innerText = newsContent;
-  pNews.classList.add('news-appear');
-  var newsSelector = document.querySelector('.news');
-  var currentNews = newsSelector.querySelector('p');
-  currentNews.classList.add('news-disapear');
-  newsSelector.append(pNews);
-  setTimeout(function () {
-    currentNews.parentNode.removeChild(currentNews);
-  }, 200);
+  pNews.classList.add( 'news-appear' );
+  var newsSelector = document.querySelector( '.news' );
+  var currentNews = newsSelector.querySelector( 'p' );
+  currentNews.classList.add( 'news-disapear' );
+  newsSelector.append( pNews );
+  setTimeout( function () {
+    currentNews.parentNode.removeChild( currentNews );
+  }, 200 );
 
-});
+} );
 // --------------
 
 
 
 
-function userCard(user) {
+function userCard( user ) {
   return `
   <div class="card row">
   <div class="avatar-block col-4">
@@ -102,20 +101,36 @@ function userCard(user) {
   </div>
 
   <div class="col-8 mb-0">
-    <div class="links-block">
-          <a href="${user.links[0]}" class="link"><img src="./assets/content/icons/dota.png" alt=""></a>
-          <a href="${user.links[1]}" class="link"><img src="./assets/content/icons/csgo-icon-6.png" alt=""></a>
-          <a href="${user.links[2]}" class="link"><img src="./assets/content/icons/battle.png" alt=""></a>
-          <a href="${user.links[3]}" class="link"><img src="./assets/content/icons/pubg_1337277.png" alt=""></a>
+    <div class="links-block"> 
+        ${user.links.map(icon=>getCardLink(icon)).join(' ')}
     </div>
   </div>
-  </div>`
+  </div>`;
 }
 
-function addCard(user) {
-  const cards = document.getElementById('cards');
-  const card = document.createElement('div');
-  card.classList.add('col-12', 'col-md-6', 'mb-3');
-  card.innerHTML = userCard(user);
-  cards.append(card);
+function addCard( user ) {
+  const cards = document.getElementById( 'cards' );
+  const card = document.createElement( 'div' );
+  card.classList.add( 'col-12', 'col-md-6', 'mb-3' );
+  card.innerHTML = userCard( user );
+  cards.append( card );
+}
+
+function getImage( name ) {
+  switch ( name ) {
+    case 'cs':
+      return '../assets/content/icons/csgo-icon-6.png';
+    case 'dota':
+      return '../assets/content/icons/dota.png';
+    case 'battlenet':
+      return '../assets/content/icons/battle.png';
+    case 'pubg':
+      return '../assets/content/icons/pubg_1337277.png';
+    default:
+      return null;
+  }
+}
+
+function getCardLink(icon){
+  return `<a href="${icon.link}" class="link"><img src="${getImage(icon.name)}" alt=""></a>`;
 }
